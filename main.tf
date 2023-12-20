bb673bebaa0f686cd723753d242d7d34ef03d9ab
Yair Rodriguez  @g0rducci <64326986+g0rducci@users.noreply.github.com>
2023-12-20 14:54:32 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -54,3 +54,26 @@ resource "aws_s3_object" "upload_object" {
   etag          = filemd5("html/${each.value}")
   content_type  = "text/html"
 }
+
+resource "aws_s3_bucket_policy" "read_access_policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.bucket.arn}",
+        "${aws_s3_bucket.bucket.arn}/*"
+      ]
+    }
+  ]
+}
+POLICY
+}
