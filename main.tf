b1f344cad108c52caf5fddfbfd91f6398a9c5eb9
Yair Rodriguez  @g0rducci <64326986+g0rducci@users.noreply.github.com>
2023-12-20 14:32:43 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,59 +16,32 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-west-2"
+  region     = "us-east-1"
 }
-
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
+############ Creating a Random String ############
+resource "random_string" "random" {
+  length = 6
+  special = false
+  upper = false
+} 
+############ Creating an S3 Bucket ############ 
+resource "aws_s3_bucket" "bucket" {
+  bucket = "whizbucket-${random_string.random.result}"
+  force_destroy = true
 }
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_s3_bucket_website_configuration" "blog" {
+  bucket = aws_s3_bucket.bucket.id
+  index_document {
+    suffix = "index.html"
   }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  error_document {
+    key = "error.html"
   }
 }
-
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
